38cb16d7b8d7899cbcce588f380440641d61393e
sofiamusacchio9 <160505667+sofiamusacchio9@users.noreply.github.com>
2024-10-07 10:13:48 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 function criaCartao(categoria, pergunta, resposta) {
-    console.log(categoria, pergunta, resposta)
-}
-let cartao = document.createElement('article')
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
     cartao.className = 'cartao'
 
     cartao.innerHTML = `
